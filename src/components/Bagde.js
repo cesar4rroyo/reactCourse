308651b9d2b1ce1fe829dd44b113b921f9d445ab
Cesar Arroyo <cesardavid_09@hotmail.com>
2019-11-04 22:24:44 -0500

    Agregando archivos

---
+++ src/components/Bagde.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+// import confLogo from "../images/badge-header.svg";
+// eslint-disable-next-line no-unused-vars
+import "../styles/Badge.css";
+import confLogo from "../images/badge-header.svg";
+
+class Badge extends React.Component {
+    render() {
+        return (
+            <div className="Badge">
+                <div className="Badge__header">
+                    <img src={confLogo} alt="Logo de la conferencia" />
+                </div>
+                <div className="Badge__section-name">
+                    <img
+                        className="Bagde__avatar"
+                        src="https://es.gravatar.com/cesar4rroyo"
+                        alt="Avatar"
+                    />
+                    <h1>
+                        Cesar <br /> Arroyo
+                    </h1>
+                </div>
+                <div className="Badge__section-info">
+                    <h3>Frontend Engineer</h3>
+                    <div>@cesar4rroyo</div>
+                </div>
+                <div className="Badge__footer">#platziConf</div>
+            </div>
+        );
+    }
+}
+
+export default Badge;
